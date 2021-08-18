9430b5ed45947cc6122167ca39bb53bcfbbf2011
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-18 16:22:35 -0400

    update video

---
+++ Develop/config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('ecommerce_db', 'root', 'Password', {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
